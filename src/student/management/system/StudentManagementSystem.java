4dfd9bc8998fe3ba03d12352b82545cd0d092a4b
Prakritivedi12 <148558920+Prakritivedi12@users.noreply.github.com>
2023-12-22 10:44:44 +0530

    Add files via upload

---
+++ src/student/management/system/StudentManagementSystem.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.management.system;
+
+/**
+ *
+ * @author samee
+ */
+public class StudentManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       {Welcome ab= new Welcome();
+            ab.setVisible(true);
+       }
+    
+}
+    
+}
